4bfc3fe22068f89872d3363281bc03f1fec1183b
Hernandezsson <160730061+Hernandezsson@users.noreply.github.com>
2025-10-16 10:54:22 +0200

    Delete src directory

--- src/FirebaseFunctions/functions/src/index.js
+++
@@ -1,8 +0,0 @@
-// Importamos las funciones del servidor REST
-
-const { ManejarGET } = require("./servidorREST/ManejarGET");
-const { ManejarPOST } = require("./servidorREST/ManejarPOST");
-
-// Exportamos las funciones para Firebase
-exports.ManejarPOST = ManejarPOST;
-exports.ManejarGET = ManejarGET;
